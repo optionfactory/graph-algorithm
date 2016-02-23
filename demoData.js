b571a269f8a86491034f5fffc5718760970c1900
Davide Salvador <laevar@gmail.com>
2016-02-23 19:49:04 +0100

    :fix: renamed main file to better reflect its contents, added single node scenario

---
+++ demoData.js
@@ -0,0 +1,23 @@
+var nodeExample = {
+	id: justAnyString,
+	timestamp: 0,
+	parents:[]
+}
+
+var graphExamples={
+	empty:[],
+	singleNode:[{
+		id: 2c77781,
+		timestamp: 0,
+		parents:[]
+	}],
+	twoNodes:[{
+		id: 2c77781,
+		timestamp: 0,
+		parents:[]
+	}, {
+		id: 2c77781,
+		timestamp: 0,
+		parents:[]
+	}]
+}
