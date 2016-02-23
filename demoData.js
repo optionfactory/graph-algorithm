3a5b6d3958afd8d00b607b95e0b85165d49f5aa4
Davide Salvador <laevar@gmail.com>
2016-02-23 19:55:41 +0100

    :new: three nodes, better formatting

--- demoData.js
+++ demoData.js
@@ -5,20 +5,21 @@ var nodeExample = {
 }
 
 var graphExamples={
-	empty:[],
-	singleNode:[{
-		id: 2c77781,
-		timestamp: 0,
-		parents:[]
-	}],
-	twoNodes:[{
-		id: 2c77781,
-		timestamp: 0,
-		parents:[]
-	}, {
-		id: b571a26,
-		timestamp: 1,
-		parents:[2c77781]
-	}]]
-
+	empty:[
+	],
+	singleNode:[
+		{ id: "2c77781", timestamp: 0,	parents:[] }
+	],
+	twoNodes:[
+		{ id: "2c77781", timestamp: 0,	parents:[] }
+		{ id: "b571a26", timestamp: 1,	parents:["2c77781"] }
+	],
+	threeNodes:[
+		{ id: "2c77781", timestamp: 0,	parents:[] }
+		{ id: "b571a26", timestamp: 1,	parents:["2c77781"] }
+		{ id: "3295c5a", timestamp: 2,	parents:["b571a26"] }
+	]
 }
+
+
+
