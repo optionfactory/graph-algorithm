e9b9b008db74c0e06da916445850c90c24b68430
Davide Salvador <davide@optionfactory.net>
2016-02-24 09:17:51 +0100

    :enh: more demo cases, using clearer ids

--- demoData.js
+++ demoData.js
@@ -8,16 +8,59 @@ var graphExamples={
 	empty:[
 	],
 	singleNode:[
-		{ id: "2c77781", timestamp: 0,	parents:[] }
+		{ id: "root", 	timestamp: 0, parents:[] }
 	],
 	twoNodes:[
-		{ id: "2c77781", timestamp: 0,	parents:[] },
-		{ id: "b571a26", timestamp: 1,	parents:["2c77781"] }
+		{ id: "root", 	timestamp: 0, parents:[] },
+		{ id: "tip", 	timestamp: 1, parents:["root"] }
 	],
 	threeNodes:[
-		{ id: "2c77781", timestamp: 0,	parents:[] },
-		{ id: "b571a26", timestamp: 1,	parents:["2c77781"] },
-		{ id: "3295c5a", timestamp: 2,	parents:["b571a26"] }
+		{ id: "root", 	timestamp: 0, parents:[] },
+		{ id: "node1", 	timestamp: 1, parents:["root"] },
+		{ id: "tip", 	timestamp: 2, parents:["node1"] }
+	],
+	fourNodes:[
+		{ id: "root", 	timestamp: 0, parents:[] },
+		{ id: "node1", 	timestamp: 1, parents:["root"] },
+		{ id: "node2", 	timestamp: 2, parents:["node1"] },
+		{ id: "tip", 	timestamp: 3, parents:["node2"] }
+	],
+	forkAtRoot:[
+		{ id: "root", 	timestamp: 0, parents:[] },
+		{ id: "br1", 	timestamp: 1, parents:["root"] },
+		{ id: "br2", 	timestamp: 2, parents:["root"] }
+	],
+	forkAtFirstLeaf:[
+		{ id: "root", 	timestamp: 0, parents:[] },
+		{ id: "node1", 	timestamp: 1, parents:["root"] },
+		{ id: "br1", 	timestamp: 2, parents:["node1"] },
+		{ id: "br2", 	timestamp: 3, parents:["node1"] }
+	],
+	forkAndMerge:[
+		{ id: "root", 	timestamp: 0, parents:[] },
+		{ id: "br1", 	timestamp: 1, parents:["root"] },
+		{ id: "br2", 	timestamp: 2, parents:["root"] },
+		{ id: "tip", 	timestamp: 3, parents:["br1", "br2"] }
+	],
+	forkAtRootLongerBranch:[
+		{ id: "root", 	timestamp: 0, parents:[] },
+		{ id: "br1n1", 	timestamp: 1, parents:["root"] },
+		{ id: "br2", 	timestamp: 2, parents:["root"] },
+		{ id: "br1n2", 	timestamp: 1, parents:["br1n1"] }
+	],
+	forkAtFirstLeafLongerBranch:[
+		{ id: "root", 	timestamp: 0, parents:[] },
+		{ id: "node1", 	timestamp: 1, parents:["root"] },
+		{ id: "br1n1", 	timestamp: 2, parents:["node1"] },
+		{ id: "br2", 	timestamp: 3, parents:["node1"] },
+		{ id: "br1n2", 	timestamp: 1, parents:["br1n1"] }
+	],
+	forkAndMergeLongerBranch:[
+		{ id: "root", 	timestamp: 0, parents:[] },
+		{ id: "br1n1", 	timestamp: 2, parents:["root"] },
+		{ id: "br2", 	timestamp: 2, parents:["root"] },
+		{ id: "br1n2", 	timestamp: 3, parents:["br1n1"] },
+		{ id: "tip", 	timestamp: 4, parents:["br1n2", "br2"] }
 	]
 }
 
