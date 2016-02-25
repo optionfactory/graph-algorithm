50db3c482e7c2a85f097624a7bfd60a612e395fb
Davide Salvador <davide@optionfactory.net>
2016-02-25 10:39:48 +0100

    :enh: better tests

--- tests/dijkstraPathTests.js
+++ tests/dijkstraPathTests.js
@@ -17,16 +17,33 @@ var deepEqual = function(x, y) {
         return true;
 }
 
+function errorMessage(possibleExpectedSolutions, gotSolution) {
+    var msg = "exptected: ";
+    if (possibleExpectedSolutions.length < 2) {
+        msg = msg + "\"" + possibleExpectedSolutions + "\"";
+    } else {
+        msg = msg + " either \"" + possibleExpectedSolutions[0] + "\"";
+        msg = msg + possibleExpectedSolutions.splice(1).map(function(expected) {
+            return " or \"" + expected + "\"";
+        });
+    }
+    msg += "; got: \"" + gotSolution + "\"";
+    return msg;
+}
+
 QUnit.module("DijkstraPath.shortestToTip");
 QUnit.test("path to not existing node is empty", function(assert) {
     var graph = demoGraphs.singleNode;
     assert.deepEqual(DijkstraPath.shortestToNode(graph.nodes, "notExisting"), []);
 });
+
 Object.keys(demoGraphs).forEach(function(graphName) {
     QUnit.test(graphName, function(assert) {
         var graph = demoGraphs[graphName];
         var got = DijkstraPath.shortestToNode(graph.nodes, "tip");
-        assert.ok(graph.shortestToTip.some(function(expected){return deepEqual(expected,got)}), "expected: \""+graph.shortestToTip+"\",got: \""+got+"\"");
+        assert.ok(graph.shortestToTip.some(function(expected) {
+            return deepEqual(expected, got)
+        }), errorMessage(graph.shortestToTip, got));
     });
 });
 
@@ -39,7 +56,9 @@ Object.keys(demoGraphs).forEach(function(graphName) {
     QUnit.test(graphName, function(assert) {
         var graph = demoGraphs[graphName];
         var got = DijkstraPath.longestToNode(graph.nodes, "tip");
-        assert.ok(graph.longestToTip.some(function(expected){return deepEqual(expected,got)}), "expected: \""+graph.longestToTip+"\",got: \""+got+"\"");
+        assert.ok(graph.longestToTip.some(function(expected) {
+            return deepEqual(expected, got)
+        }), errorMessage(graph.longestToTip, got));
     });
 });
 
@@ -48,6 +67,8 @@ Object.keys(demoGraphs).forEach(function(graphName) {
     QUnit.test(graphName, function(assert) {
         var graph = demoGraphs[graphName];
         var got = DijkstraPath.longestPossible(graph.nodes);
-        assert.ok(graph.longestPossible.some(function(expected){return deepEqual(expected,got)}), "expected: \""+graph.longestPossible+"\",got: \""+got+"\"");
+        assert.ok(graph.longestPossible.some(function(expected) {
+            return deepEqual(expected, got)
+        }), errorMessage(graph.longestPossible, got));
     });
 });
