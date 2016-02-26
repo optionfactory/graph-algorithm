4fefb1ac83dbececf3deddeedf62714d01b81af4
Davide Salvador <davide@optionfactory.net>
2016-02-26 09:22:31 +0100

    :new: no-collisions test

--- tests/coordinatesCalculationTests.js
+++ tests/coordinatesCalculationTests.js
@@ -1,13 +1,25 @@
 QUnit.module("Coordinates.Calculation");
-Object.keys(demoGraphs).forEach(function(graphName){
-	console.log(graphName);
-    QUnit.test("all " + graphName + " nodes have coordinates", function(assert) {
+Object.keys(demoGraphs).forEach(function(graphName) {
+    QUnit.test(graphName + ": all nodes have coordinates", function(assert) {
         var graph = demoGraphs[graphName];
         var coords = fixNodes(graph.nodes, 0, 0);
         var untouchedNodes = coords.filter(function(node) {
             return node.x === undefined && node.y === undefined;
         });
-        console.log(graph, coords, untouchedNodes, untouchedNodes.length);
-        assert.deepEqual(untouchedNodes.length, 0);
+        assert.equal(untouchedNodes.length, 0);
+    });
+    QUnit.test(graphName + ": no two nodes on the same coordinates", function(assert) {
+        var graph = demoGraphs[graphName];
+        var coords = fixNodes(graph.nodes, 0, 0);
+        var nodesAndClashes = coords.map(function(node) {
+            return {
+                node: node,
+                clashingNodesIds: coords.filter(function(otherNode) {
+                    //search nodes with different id, but same coordinates
+                    return otherNode.id !== node.id && otherNode.x === node.x && otherNode.y === node.y;
+                }).map(graphs.toId)
+            }
+        })
+        assert.ok(!nodesAndClashes.some(function(nodeAndClashes){return nodeAndClashes.clashingNodesIds.length!==0}))
     });
 })
